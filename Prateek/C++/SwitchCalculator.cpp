55097f39ddf39a5451a8bd29b5074b0444e2f108
DaiyaCodes <dakshgotsandalphon@gmail.com>
2024-12-25 21:43:55 +0530

    Folder Rename

---
+++ Prateek/C++/SwitchCalculator.cpp
@@ -0,0 +1,84 @@
+// #include<iostream>
+// using namespace std; 
+
+//     int main(){
+
+//         int a,b;
+//         char ch;
+
+//         cout<<"Enter the value of a : ";
+//         cin>>a;
+
+//         cout<<"Enter the value of b : ";
+//         cin>>b;
+        
+//         cout<<"Enter the operation you want to perform : ";
+//         cin>>ch;
+
+//         switch(ch){
+
+//             case '+' : cout<<a+b;
+//             break;
+
+//             case '-' : cout<<a-b;
+//             break;
+
+//             case '/' : cout<<a/b;
+//             break;
+
+//             case '*' :  cout<<a*b;
+//             break;
+
+//             case '%' : cout<<a%b;
+//             break; 
+
+//             default : cout<<"Enter the correct operator.";
+
+
+//         }
+        
+
+//     return 0;
+// }
+
+
+// *********************QUESTION********************* 
+
+// You have 1330 rupees. How many 100,50,20,1 rupees notes are required ?
+
+// #include<iostream>
+// using namespace std; 
+
+//     int main(){
+//     int n;
+//     cout<<"Enter the amount : ";
+//     cin>>n;
+
+//     if (n/100!=0){
+
+//         cout<<"100 rupees notes required are : "<<n/100<<endl;
+//         n=n%100;
+//     }
+//     if(n/50!=0){
+
+//         cout<<"50 rupees notes required are : "<<n/50<<endl;
+//         n = n%50;
+//     }
+//     if (n/20!=0){
+
+//         cout<<"20 rupees notes required are : "<<n/20<<endl;
+//         n=n%20;
+//     }
+//     if(n/1!=0){
+//         cout<<"1 rupees coins required are : "<<n/1;
+//     }
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    cout<<pow(10,5);
+}
