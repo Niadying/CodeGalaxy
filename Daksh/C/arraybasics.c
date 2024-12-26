3ab06ea033fa4821cb2bc9ad10cf2c9d37645fb6
DaiyaCodes <dakshgotsandalphon@gmail.com>
2024-12-26 17:57:04 +0530

    hmph

--- Daksh/C/arraybasics.c
+++ Daksh/C/arraybasics.c
@@ -2,5 +2,16 @@
 
 int main(){
 
-    int 
+    int works[12];
+
+    for(int i = 0; i < 12; i++){
+        scanf("%d", works[i]);
+    }
+
+    for(int i = 0; i < 12; i++){
+        printf("Value %d: %d\n", i+1, works[i]);
+    }
+
+    return 0;
+
 }
