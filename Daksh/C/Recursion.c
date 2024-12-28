d633aa2bea635989fee8f917508a56e9748d9dee
DaiyaCodes <dakshgotsandalphon@gmail.com>
2024-12-28 17:40:59 +0530

    Sleepy as Fk

--- Daksh/C/Recursion.c
+++ Daksh/C/Recursion.c
@@ -33,6 +33,8 @@ int main(){
 }
 
 int factorial(int n){
+
     if(n == 1) return 1;
     return n * factorial(n - 1);
+
 }
---
+++ Daksh/C/typedef.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef int KM;
+
+int main(){
+
+    KM Bhabra, Alirajpur, Bhopal;
+
+    Bhabra = 2;
+    Bhopal = 354;
+    Alirajpur = 28;
+
+    printf("Bhabra: %d\n", Bhabra);
+    printf("Bhopal: %d\n", Bhopal);
+    printf("Alirajpur: %d\n", Alirajpur);
+
+    return 0;
+
+}
