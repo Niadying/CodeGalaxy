ca5d1e992b57ab4764c959a344d15fc3ca4790bf
DaiyaCodes <dakshgotsandalphon@gmail.com>
2024-12-28 17:11:53 +0530

    I dont get it

---
+++ Daksh/C/Recursion.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(){
+
+    //! Not Recursion
+
+    int n = 0;
+    int fact = 1;
+
+    printf("Enter A Number: ");
+    scanf("%d", &n);
+
+    /*
+
+    while (n >= 1)
+    {
+        fact = n * fact;
+        n--;
+    }
+
+    */
+
+    //! In recursion
+
+    fact = factorial(n);
+
+    printf("Factorial : %d\n", fact);
+
+    return 0;
+
+}
+
+int factorial(int n){
+    if(n == 1) return 1;
+    return n * factorial(n - 1);
+}
