83504577077ffafd4f0da95e788c840f003abafd
dev-PrateekSahu <prateeksahu75116@gmail.com>
2025-01-03 16:08:06 +0530

    Trying out something

--- Prateek/C++/Arrays/StringsAndCharArray.cpp
+++ Prateek/C++/Arrays/StringsAndCharArray.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    string f = "Prateek Sahu";
-    cout<<f<<endl;
-    string ps;
-    cin>>ps;
-    cout<<ps;
-}
+
+    // char ch[20];
+    // cin>>ch;  // AFTER TAKING I/P IF STRING REMAINS EMPTY SO A NULL CHARACTER WILL BE STORED AND IT WILL TELL THAT STRING IS OVER AND WHILE PRINTING IT WILL NOT PRINT FURTHER..
+    // for(int i  = 0  ;i<20 ;i++){
+    //     cout<<ch[i];
+    // }
+
+    vector<int>v;
+    for(int i = 0; i<5; i++){
+        cin>>v[i];
+    }
+
+    for(int i = 0; i<5 ; i++){
+        cout<<v[i]<<" ";
+    }
+
+}   
