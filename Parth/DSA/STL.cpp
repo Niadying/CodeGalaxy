23d0e9134ffc7e63395183ff56ef184f5e1b2758
Parth <parthpsharma22@gmail.com>
2025-01-03 15:49:52 +0530

    STL updated

--- Parth/DSA/STL.cpp
+++ Parth/DSA/STL.cpp
@@ -1,32 +1,63 @@
+// #include <iostream>
+// #include<utility>
+
+// using namespace std;
+
+// void explainpair()
+// {
+//     pair<int,int> p = {1,3};
+
+//     cout << p.first<< "   "<<p.second;
+
+//     pair<int, pair<int, int>> q= {1, {3, 4}};
+
+//    cout << q.first << " " << q.second.second<<" "<< q.second.first<<endl;
+    
+// }
+
+
+// int main()
+// {
+    
+// explainpair();
+
+
+//     return 0; 
+// }
+
+
+// void explainpair() {
+//     pair<int, int> p = make_pair(1, 3); // Correct initialization
+//     cout << p.first << "   " << p.second << endl;
+
+//     pair<int, pair<int, int> > q = make_pair(1, make_pair(3, 4)); // Nested pair with make_pair
+//     cout << q.first << " " << q.second.second << " " << q.second.first << endl;
+//     pair<int, int> arr[] =  make_pair{{1,2},{2,5},{5,1}} ;
+// }
+
+// int main() {
+//     explainpair();
+//     return 0;
+// }
+
+
 #include <iostream>
+#include <utility>
+#include<vector>
 
 using namespace std;
 
-void explainpair()
+void explainvector()
 {
-    pair<int,int> p= {1,3};
+      vector<int> v;
 
-    cout << p.first<< "   "<<p.second;
+      v.push_back(1);
+      v.push_back(2);
 
-    pair <int, pair<int, int >>p= {1, {3, 4}};
-
-   // cout << p.first << " " << p.second.second<<" "<< p.second.first<<endl;
-
-    
+      cout<< v[1]<< endl;
 }
-
 
 int main()
 {
-    
-explainpair();
-
-
-
-
-
-
-
-
-    return 0; 
+    explainvector();
 }
---
+++ Parth/DSA/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ pair<int,int> p = {1,3};
+
+    cout << p.first<< "   "<<p.second;
