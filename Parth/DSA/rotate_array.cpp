62d5cd79c9565197c61fa39b2b0e4528938f3807
Parth <parthpsharma22@gmail.com>
2025-01-07 23:02:42 +0530

    rotate arrays done

--- Parth/DSA/rotate_array.cpp
+++ Parth/DSA/rotate_array.cpp
@@ -3,31 +3,72 @@
 
 using namespace std;
 
-int main()
-{   int n;
-    cout <<"enter no. of elements :  ";
-    cin>>n;
+//BY 1 PLACE!!!
 
-    vector<int> arr(n);
+// int main()
+// {   int n;
+//     cout <<"enter no. of elements :  ";
+//     cin>>n;
 
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+//     vector<int> arr(n);
 
-    int temp = arr[0];
-    for(int i=1;i<n;i++){
-        arr[i-1]=arr[i];
-    }
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     int temp = arr[0];
+//     for(int i=1;i<n;i++){
+//         arr[i-1]=arr[i];
+//     }
      
-     arr[n-1]=temp;
+//      arr[n-1]=temp;
+
+//      for(int i=0;i<n;i++){
+//         cout<<arr[i]<<" ";
+       
+//     }
+
+//BY K PLACES!!!
+
+
+void leftRotate(int arr[], int n, int k){
+    reverse(arr,arr+k);
+    reverse(arr+k,arr+n);
+    reverse(arr,arr+n);
+
+
+}
+
+int main(){
+     int n;
+     cout <<"enter no. of elements :  ";
+     cin>>n;
+
+     int arr[n];
 
      for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
-    }
+     cin>>arr[i];
+     }
 
+     int k;
+     cin>>k;
+
+     leftRotate(arr,n,k);
+
+    for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+       
+    }
 
 
 
 
 
 }
+
+
+
+
+
+
+
