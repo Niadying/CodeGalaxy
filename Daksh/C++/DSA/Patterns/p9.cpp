592f9f7508777483fc7cd3c4901c2be337182d87
DaiyaCodes <dakshgotsandalphon@gmail.com>
2025-01-24 15:39:34 +0530

    some patterns

--- Daksh/C++/DSA/Patterns/p9.cpp
+++ Daksh/C++/DSA/Patterns/p9.cpp
@@ -18,6 +18,18 @@ int main(){
         }
         cout << endl;
     }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j < 2*n - (2*i+1); j++){
+            cout << "*";
+        }
+        for(int j = 0; j <i; j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 
