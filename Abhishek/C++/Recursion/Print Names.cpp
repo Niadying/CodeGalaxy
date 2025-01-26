b6b578bf55d7690518996dc7e87135c172cfff03
Abhihabibi <xing12505@gmail.com>
2025-01-26 21:30:32 +0530

    recursion started

--- Abhishek/C++/Recursion/Print Names.cpp
+++ Abhishek/C++/Recursion/Print Names.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void printname(int n , int c){
-    
+void printname(int c , int n){
+    cout<<"name"<<endl;
+    c++;
+    if(c<n){
+        printname(c,n);
+
+    }
+
+
+}
+
+int main(){
+    printname(1,7);
 }
