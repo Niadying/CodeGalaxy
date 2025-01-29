f13709410e237cf43fe10e4737969ba5f52e32ee
Abhihabibi <xing12505@gmail.com>
2025-01-29 13:02:41 +0530

    finally arrays

---
+++ Abhishek/C++/Sorting/quick_aort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void quick(int arr[], int low, int high){
+    int left=low+1;
+    int right=high;
+    if (left = right){
+        return;
+    }
+    int pivot=low;
+
+    while(left<right){
+        if(arr[left]<)
+
+    }
+
+}
+
+
+int main(){
+    cout<<"arey yrr ye poora tim kha gyi ";
+}
