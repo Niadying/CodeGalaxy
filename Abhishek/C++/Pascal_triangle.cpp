f5f7331148c79757f0fb72348c392f1fb5df51c6
DaiyaCodes <dakshgotsandalphon@gmail.com>
2025-02-06 12:49:46 +0530

    abhishek use krra tha mera pc

---
+++ Abhishek/C++/Pascal_triangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int fact(int a ){
+    if(a==0||a==1) return 1;
+    return a*fact(a-1);
+}
+int term(int b,int a){
+    return fact(a)/(fact(b)*fact(a-b));
+}
+
+
+int main(){
+    cout<<"Enter the number of rows you wnt to print: ";
+    int rows;
+    cin>>rows;
+    for(int i=0;i<rows ;i++){
+        for (int j=0;j<rows-i;j++)cout<<" ";
+        for(int j=0;j<=i;j++)cout<<term(j,i)<<"  ";
+        cout<<endl;
+    }
+    int a ,b;
+    
+}
