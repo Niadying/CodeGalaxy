55a63db983ab6b7c1120bc69660e1350e79f1182
Parth <parthpsharma22@gmail.com>
2025-05-30 18:45:33 +0530

    spiti vallleyyyy

--- Parth/DSA/Recursion/subsequence.cpp
+++ Parth/DSA/Recursion/subsequence.cpp
@@ -1,25 +1,84 @@
 #include<iostream>
 using namespace std;
 
-void print(int ind,vector<int> &ds,int arr[],int n ){
+//print the subsequences whose sum is k
+
+// bool print(int ind,vector<int> &ds,int s,int sum, int arr[],int n){
+   
+//     if(ind==n){
+//         if (s==sum){
+//         for(auto it: ds){
+//             cout<<it<<" ";
+//         }  
+//         cout<<endl;
+//         return true;
+
+//     }
+//         else return false ;
+//     }
+    
+//     ds.push_back(arr[ind]);
+//     s +=arr[ind];
+
+//     if (print(ind+1,ds,s,sum,arr,n)==true){
+//         return true;
+//     }
+
+//     s -= arr[ind];
+//     ds.pop_back();
+
+//      if (print(ind+1,ds,s,sum,arr,n)==true) return true; 
+
+//      return false;
+//     }
+
+
+// int main(){
+//     int arr[]={1,2,1};
+//     int n=3;
+//     int sum=2;
+//     vector<int> ds; 
+//     print(0,ds,0,sum,arr,n);
+// }
+
+
+//count the subsequencees with sum k
+
+
+
+int print(int ind,vector<int> &ds,int s,int sum, int arr[],int n){
+   
     if(ind==n){
+        if (s==sum){
         for(auto it: ds){
             cout<<it<<" ";
-        }
+        }  
         cout<<endl;
-        return;
-    }
+        return 1;
 
+    }
+        else return 0 ;
+    }
+    
     ds.push_back(arr[ind]);
-    print(ind+1,ds,arr,n);
+    s +=arr[ind];
+
+    int l=print(ind+1,ds,s,sum,arr,n);
+
+
+    s -= arr[ind];
     ds.pop_back();
 
-     print(ind+1,ds,arr,n);
+     int r= print(ind+1,ds,s,sum,arr,n);
+
+     return l+r;
+    
 }
 
 int main(){
-    int arr[]={3,1,2};
+    int arr[]={1,2,1};
     int n=3;
-    vector<int> ds;
-    print(0,ds,arr,n);
+    int sum=2;
+    vector<int> ds; 
+    cout<<print(0,ds,0,sum,arr,n);
 }
